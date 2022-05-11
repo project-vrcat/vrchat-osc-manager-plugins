58d858d40626f880e7bd4b59eb3aff950103f170
Gizmo <cunyu@liuli.lol>
2022-05-11 20:12:34 +0800

    feat: deno module添加 `listenParameters`

--- module/deno/vrchat-osc-manager.ts
+++ module/deno/vrchat-osc-manager.ts
@@ -1,27 +1,38 @@
 // deno-lint-ignore-file no-explicit-any
 import { green } from "https://deno.land/std@0.129.0/fmt/colors.ts";
+import { EventEmitter } from "https://deno.land/x/event@2.0.0/mod.ts";
 
 export const pluginName = Deno.env.get("VRCOSCM_PLUGIN");
 
-interface Pool {
-  resolve: (value: void | PromiseLike<void>) => void;
-  reject: (reason?: any) => void;
-  pp: (data: any) => any;
+type value = string | number | boolean;
+
+type Events = {
+  parameters: [name: string, value: value[]];
+  options: [Record<string, any>];
+};
+
+interface ManagerEvent {
+  method: string;
+  plugin: string;
+  options: Record<string, any>;
+  parameterName: string;
+  parameterValue: value[];
 }
 
-export class Manager {
+export class Manager extends EventEmitter<Events> {
   public address: string;
   private ws: WebSocket | undefined;
-  private pool: Record<string, Pool>; // 不可靠, 但能用 ¯\_(ツ)_/¯
 
   constructor(
     private addr: string = Deno.env.get("VRCOSCM_WS_ADDR") ?? "localhost:8787",
   ) {
-    this.address = `ws://${this.addr}`;
-    this.pool = {};
+    super();
+    const params = new URLSearchParams();
+    params.append("plugin", pluginName!);
+    this.address = `ws://${this.addr}/?${params.toString()}`;
   }
 
-  connect(): Promise<void> {
+  public connect(): Promise<void> {
     return new Promise((resolve, reject) => {
       this.ws = new WebSocket(this.address);
       this.ws.onopen = () => {
@@ -30,10 +41,16 @@ export class Manager {
       };
       this.ws.onmessage = (event: MessageEvent) => {
         if (!event.data) return;
-        const m = JSON.parse(event.data);
-        if (m.method in this.pool) {
-          this.pool[m.method].resolve(this.pool[m.method].pp(m));
-          delete this.pool[m.method];
+        const m = JSON.parse(event.data) as ManagerEvent;
+        switch (m.method) {
+          case "get_options":
+            this.emit("options", m.options);
+            break;
+          case "parameters":
+            this.emit("parameters", m.parameterName, m.parameterValue);
+            break;
+          default:
+            break;
         }
       };
       this.ws.onerror = (err) => reject(err);
@@ -41,7 +58,7 @@ export class Manager {
     });
   }
 
-  reconnect() {
+  private reconnect() {
     const old = this.ws!;
     console.log(green("Manager Reconnect"));
     this.ws = new WebSocket(this.address);
@@ -51,23 +68,26 @@ export class Manager {
   }
 
   private wsSend(data: any) {
-    if (this.ws?.readyState === WebSocket.OPEN) {
+    (this.ws?.readyState === WebSocket.OPEN) &&
       this.ws.send(JSON.stringify(data));
-    }
   }
 
-  getOptions(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this.pool["get_options"] = {
-        resolve,
-        reject,
-        pp: (data: any) => data.options,
-      };
+  getOptions(): Promise<Record<string, any>> {
+    return new Promise((resolve) => {
+      this.once("options", (options) => resolve(options));
       this.wsSend({ method: "get_options", plugin: pluginName });
     });
   }
 
   send(addr: string, value: string | number | boolean) {
     this.wsSend({ method: "send", plugin: pluginName, addr, value });
+  }
+
+  listenParameters(p: string[]) {
+    this.wsSend({
+      method: "listen_parameters",
+      plugin: pluginName,
+      parameters: p,
+    });
   }
 }
